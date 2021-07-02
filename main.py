6b4b7ae3390c6a112f108148d753204229ad1a0d
gowhale <gabewhale@gmail.com>
2021-07-02 11:00:13 +0100

    Daily spends now calculated and displayed in a graph

--- main.py
+++ main.py
@@ -3,23 +3,24 @@
 
 import pandas as pd
 from datetime import datetime
+import plotly.express as px
 
 # Variables
 date_format = '%d/%m/%Y'
-start_date_string = "20/06/2021"
-end_date_string = "09/12/2021"
+start_date_string = "08/06/2021"
+end_date_string = "05/07/2021"
 
 # Load CSV into dataframe
 raw_df = pd.read_csv("data.csv")
 
 # Create a dataframe of spends between wanted dates
 valid_dates = []
+start_date_obj = datetime.strptime(start_date_string, date_format)
+end_date_obj = datetime.strptime(end_date_string, date_format)
 
 for date in raw_df["Date"]:
 
     date_obj = datetime.strptime(date, date_format)
-    start_date_obj = datetime.strptime(start_date_string, date_format)
-    end_date_obj = datetime.strptime(end_date_string, date_format)
 
     if not (date_obj < start_date_obj) and not (end_date_obj < date_obj):
         valid_dates.append(True)
@@ -44,6 +45,47 @@ for line in unwanted_transactions_lines:
         
 trimmed_df.to_csv('transaction_log.csv')
 
+# Create a daily expenses dataframe
+
+expenses = trimmed_df[trimmed_df["Amount"]<0]
+income = trimmed_df[trimmed_df["Amount"]>0]
+
+list_of_date_range = pd.date_range(start=start_date_obj, end=end_date_obj)
+list_of_date_range = list_of_date_range.strftime(date_format)
+
+daily_dates = []
+daily_sums = []
+
+for date in list_of_date_range:
+    daily_dates.append(date)
+    date_sum = expenses[expenses["Date"] == date]["Amount"].sum()
+    daily_sums.append(date_sum)
+    print((date,date_sum))
+
+daily_expenses_df = pd.DataFrame(
+    {'Date': daily_dates,
+     'Amount': daily_sums,
+    })
+
 # Create graph for daily spending
+
+print("\n\nCreating graph...")
+fig = px.scatter(daily_expenses_df, x="Date", y="Amount",color="Amount",color_continuous_scale=[ "red", "orange","green",],)
+fig.update_traces(marker=dict(size=12))
+fig.update_yaxes(range=[0, -200])
+fig.update_layout(
+    title="Sleep Scores Over Time",
+    xaxis_title="Date",
+    yaxis_title="Overall Sleep Score",
+    font=dict(
+        family="Arial",
+        size=25,
+        color="#7f7f7f"
+    )
+)
+
+print("Graph created.")
+fig.show()
+print("Graph saved.")
 
 # Calculate average daily spend
