f9bf43129b4d338a9e03bef45f3e55f9b7cb970c
gowhale <gabewhale@gmail.com>
2021-07-02 11:22:37 +0100

    Average daily spend now outputted and minor variable name changes

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import pandas as pd
 from datetime import datetime
 import plotly.express as px
 
+
 # Variables
 date_format = '%d/%m/%Y'
-start_date_string = "08/06/2021"
-end_date_string = "05/07/2021"
+start_date_string = "25/06/2021"
+end_date_string = "01/07/2021"
+
 
 # Load CSV into dataframe
 raw_df = pd.read_csv("data.csv")
+
 
 # Create a dataframe of spends between wanted dates
 valid_dates = []
@@ -35,18 +38,19 @@ spending_within_dates_df = raw_df[raw_df["Within Wanted Range"] == True]
 # Remove unwanted transactions
 trimmed_df = spending_within_dates_df
 ignore_transation_file = open('unwanted_transactions.txt', 'r')
-unwanted_transactions_lines = ignore_transation_file.readlines()
+unwanted_transactions = ignore_transation_file.readlines()
 
-for line in unwanted_transactions_lines:
-    stripped_line = (line.strip())
-    if stripped_line[:3] == "tx_":
-        print(stripped_line)
-        trimmed_df = trimmed_df[trimmed_df["Transaction ID"] != stripped_line]
+for transaction in unwanted_transactions:
+    stripped_transaction = (transaction.strip())
+    if stripped_transaction[:3] == "tx_":
+        print(stripped_transaction)
+        trimmed_df = trimmed_df[trimmed_df["Transaction ID"]
+                                != stripped_transaction]
 
 trimmed_df.to_csv('trimmed_dataset.csv')
 
-# Create a daily expenses dataframe
 
+# Create a daily expenses dataframe
 expenses = trimmed_df[trimmed_df["Amount"] < 0]
 expenses["Amount"] = expenses["Amount"].abs()
 income = trimmed_df[trimmed_df["Amount"] > 0]
@@ -55,23 +59,23 @@ list_of_date_range = pd.date_range(start=start_date_obj, end=end_date_obj)
 list_of_date_range = list_of_date_range.strftime(date_format)
 
 daily_dates = []
-daily_sums = []
+daily_expenses_sums = []
 
 for date in list_of_date_range:
     daily_dates.append(date)
     date_sum = expenses[expenses["Date"] == date]["Amount"].sum()
-    daily_sums.append(date_sum)
+    daily_expenses_sums.append(date_sum)
     print((date, date_sum))
 
 daily_expenses_df = pd.DataFrame(
     {'Date': daily_dates,
-     'Amount': daily_sums,
+     'Amount': daily_expenses_sums,
      })
 
 daily_expenses_df.to_csv("daily_expeneses.csv")
 
-# Create graph for daily spending
 
+# Create graph for daily spending
 print("\n\nCreating graph...")
 fig = px.scatter(daily_expenses_df, x="Date", y="Amount", color="Amount",
                  color_continuous_scale=["green", "orange", "red", ],)
@@ -92,4 +96,11 @@ print("Graph created.")
 fig.show()
 print("Graph saved.")
 
+
 # Calculate average daily spend
+daily_average_spend = round(daily_expenses_df["Amount"].mean(), 2)
+
+print("\n\n\n")
+print("The average daily spend between {} and {} is £{}".format(
+    start_date_string, end_date_string, daily_average_spend))
+print("\n\n\n")
